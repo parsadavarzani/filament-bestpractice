fcf9e127e6866981bb435b4ee4f3d5cc9e9575d6
parsa <p.davarzani3@gmail.com>
2024-10-09 10:31:26 +0330

    Config TreatmentChart

--- app/Filament/Widgets/TreatmentsChart.php
+++ app/Filament/Widgets/TreatmentsChart.php
@@ -2,7 +2,10 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Treatment;
 use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class TreatmentsChart extends ChartWidget
 {
@@ -10,8 +13,22 @@ class TreatmentsChart extends ChartWidget
 
     protected function getData(): array
     {
+        $data = Trend::model(Treatment::class)
+            ->between(
+                start: now()->subYear(),
+                end: now(),
+            )
+            ->perMonth()
+            ->count();
+
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Treatments',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
         ];
     }
 
