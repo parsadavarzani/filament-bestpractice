ae6dab8e0f33790cd55bf0c3efafb29e066895f9
parsa <p.davarzani3@gmail.com>
2024-10-09 10:18:16 +0330

    Add Relation methods to patient model

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Patient extends Model
 {
     use HasFactory;
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(Owner::class);
+    }
+
+    public function treatments(): HasMany
+    {
+        return $this->hasMany(Treatment::class);
+    }
 }
