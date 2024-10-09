91b9efddd071f62b38db89249e16a871175e6ac2
parsa <p.davarzani3@gmail.com>
2024-10-09 12:40:46 +0330

    Runs pint

--- app/Filament/Resources/PatientResource.php
+++ app/Filament/Resources/PatientResource.php
@@ -10,8 +10,6 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class PatientResource extends Resource
 {
@@ -54,7 +52,7 @@ class PatientResource extends Resource
                             ->tel()
                             ->required(),
                     ])
-                    ->required()
+                    ->required(),
             ]);
     }
 
