2c7712e112c4e623981e2f9e66fab143737931fd
parsa <p.davarzani3@gmail.com>
2024-10-09 10:27:12 +0330

    Add and Config MonyCast for handle float numbers

--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Treatment extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'price' => MoneyCast::class,
+    ];
 
     public function patient(): BelongsTo
     {
