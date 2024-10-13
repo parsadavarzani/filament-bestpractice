75561f452d716087123fb3487c63aa1ddef4d1ce
parsa <p.davarzani3@gmail.com>
2024-10-13 18:27:59 +0330

    Add relation methodes to post and author

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function categories(): MorphToMany
+    {
+        return $this->morphToMany(Category::class, 'categoryable');
+    }
+
+    public function authors(): BelongsToMany
+    {
+        return $this->belongsToMany(Author::class);
+    }
 }
