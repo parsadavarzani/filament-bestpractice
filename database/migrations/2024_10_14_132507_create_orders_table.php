52252f30c14f4b04ac007cf880353b22cc438adb
parsa <p.davarzani3@gmail.com>
2024-10-14 16:56:48 +0330

    Add Order model and migration

---
+++ database/migrations/2024_10_14_132507_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Customer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Customer::class)->constrained()->cascadeOnDelete();
+            $table->string('number', 32)->unique();
+            $table->decimal('total_price', 12, 2)->nullable();
+            $table->string('status')->default('new');
+            $table->string('currency');
+            $table->decimal('shipping_price')->nullable();
+            $table->string('shipping_method')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
