95fa05521a2e0a3b08d717c3f85d63faca590089
parsa <p.davarzani3@gmail.com>
2024-10-14 10:14:48 +0330

    Add and Config LinkResource

---
+++ app/Filament/Resources/LinkResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LinkResource\Pages;
+use App\Filament\Resources\LinkResource\RelationManagers;
+use App\Models\Link;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Support\Enums\FontWeight;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class LinkResource extends Resource
+{
+    protected static ?string $model = Link::class;
+
+    // protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Blog';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema(Link::getForm());
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\Layout\Stack::make([
+                    Tables\Columns\ImageColumn::make('image')
+                        ->height('100%')
+                        ->defaultImageUrl(url('/images/anime-girl.jpg'))
+                        ->width('100%'),
+                    Tables\Columns\Layout\Stack::make([
+                        Tables\Columns\TextColumn::make('title')
+                            ->weight(FontWeight::Bold),
+                        Tables\Columns\TextColumn::make('url')
+                            ->formatStateUsing(fn(string $state): string => str($state)->after('://')->ltrim('www.')->trim('/'))
+                            ->color('gray')
+                            ->limit(30),
+                    ]),
+                ])->space(3),
+                Tables\Columns\Layout\Panel::make([
+                    Tables\Columns\Layout\Split::make([
+                        Tables\Columns\ColorColumn::make('color')
+                            ->grow(false),
+                        Tables\Columns\TextColumn::make('description')
+                            ->color('gray'),
+                    ]),
+                ])->collapsible(),
+            ])
+            ->filters([
+                //
+            ])
+            ->contentGrid([
+                'md' => 2,
+                'xl' => 3,
+            ])
+            ->paginated([
+                18,
+                36,
+                72,
+                'all',
+            ])
+            ->actions([
+                Tables\Actions\Action::make('visit')
+                    ->label('Visit Link')
+                    ->color('gray')
+                    ->icon('heroicon-m-arrow-top-right-on-square')
+                    ->url(function (Link $record) {
+                        return "#" . urlencode($record->url);
+                    }),
+                Tables\Actions\EditAction::make()->color('info'),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLinks::route('/'),
+            'create' => Pages\CreateLink::route('/create'),
+            'edit' => Pages\EditLink::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Link.php
+++ app/Models/Link.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
     use HasFactory;
+
+    public static function getForm()
+    {
+        return [
+            TextInput::make('title')
+                ->required()
+                ->autocomplete('off')
+                ->maxLength(255),
+            TextInput::make('url')
+                ->required()
+                ->autocomplete('off')
+                ->url()
+                ->maxLength(255),
+            RichEditor::make('description')
+                ->required()
+                ->columnSpanFull(),
+            ColorPicker::make('color')
+                ->required(),
+            FileUpload::make('image')
+                ->image()
+                ->imageEditor()
+                ->directory('link')
+                ->columnSpanFull(),
+        ];
+    }
 }
