ac2314d508451673163730fb36224a04aa0b138e
parsa <p.davarzani3@gmail.com>
2024-10-15 14:27:15 +0330

    Add attriubute migration and model

---
+++ database/migrations/2024_10_15_104659_create_attribute_values_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\AttributeValue;
+use App\Models\Product;
+use App\Models\Attribute;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attribute_values', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Attribute::class)->constrained('attributes')->cascadeOnDelete();
+
+            $table->string('value');
+
+            $table->timestamps();
+        });
+
+        Schema::create('attribute_value_product', function (Blueprint $table) {
+
+            $table->foreignIdFor(AttributeValue::class)->constrained('attribute_values')->cascadeOnDelete();
+
+            $table->foreignIdFor(Product::class)->constrained('products')->cascadeOnDelete();
+
+            $table->primary(['attribute_value_id', 'product_id']);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attribute_value_product');
+        Schema::dropIfExists('attribute_values');
+    }
+};
